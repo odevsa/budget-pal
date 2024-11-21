7320c25a0396accbbc1f8efce22eaa22aa608c9c
Raphael Sá <dev.raphaelsa@gmail.com>
2024-11-20 21:02:01 -0300

    Adding active menu

--- src/app/(protected)/_components/navbar.tsx
+++ src/app/(protected)/_components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Logo from "@/components/Logo";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 interface NavBarItem {
@@ -14,6 +15,7 @@ export default function NavBar({
   menu?: NavBarItem[];
 }>) {
   const [internalMenu, setInternalMenu] = useState<NavBarItem[]>(menu ?? []);
+  const pathname = usePathname();
 
   useEffect(() => {
     if (menu) setInternalMenu(menu);
@@ -21,22 +23,26 @@ export default function NavBar({
 
   return (
     <aside className="flex flex-col w-60 bg-navbar">
-      <div className="flex flex-col px-3 py-2">
-        <ul>
-          {internalMenu.map((item) => (
-            <li key={JSON.stringify(item)}>
-              <a
-                href={item.path}
-                className={
-                  "flex flex-row gap-2 items-center rounded-md px-3 py-2 transition-all" +
-                  " hover:bg-primary hover:text-primary-foreground hover:ps-5"
-                }
-              >
-                {item.icon && item.icon}
-                {item.title}
-              </a>
-            </li>
-          ))}
+      <div className="flex flex-col p-2">
+        <ul className="flex flex-col gap-1">
+          {internalMenu.map((item) => {
+            const isActive: boolean = pathname == item.path;
+            return (
+              <li key={JSON.stringify(item)}>
+                <a
+                  href={item.path}
+                  className={cn([
+                    "flex flex-row gap-2 items-center rounded-md px-3 py-2 transition-all",
+                    "hover:bg-primary hover:text-primary-foreground hover:ps-5",
+                    isActive ? "ps-5 bg-primary text-primary-foreground" : "",
+                  ])}
+                >
+                  {item.icon && item.icon}
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="mt-auto text-center text-sm py-2 text-foreground-muted">
